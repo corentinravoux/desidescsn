42cfd5aff4624b4e3f57c058f902d44796095fa0
corentinravoux <corentin.ravoux.research@gmail.com>
2024-02-14 17:46:26 +0100

    light version of DC2 cat

---
+++ desidescsn/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+__version__ = "1.0.0"
+__desidescsn_dir_path__ = os.path.dirname(__file__)
--- desidescsn/catalogs.py
+++ desidescsn/catalogs.py
@@ -2,6 +2,8 @@ import GCRCatalogs
 import numpy as np
 import pandas as pd
 
+name_catalog_roman_rubin = "roman_rubin_2023_v1.1.1_elais"
+
 feature_list_roman_rubin = np.array(
     [
         "galaxy_id",
@@ -33,9 +35,27 @@ feature_list_roman_rubin = np.array(
     ]
 )
 
+feature_list_roman_rubin_light = np.array(
+    [
+        "ra",
+        "dec",
+        "redshift_true",
+        "mag_true_g_sdss",
+        "mag_true_r_sdss",
+        "mag_true_i_sdss",
+        "mag_true_z_sdss",
+        "ROMAN_obs_J129",
+        "ROMAN_obs_K213",
+        "sfr",
+        "stellar_mass",
+    ]
+)
 
-def extract_dc2_properties(name_catalog, feature_list, path_out):
+
+def extract_dc2_properties(name_catalog, feature_list, path_out, redshift_cut=None):
     gc = GCRCatalogs.load_catalog(name_catalog)
     dict_quantities = gc.get_quantities(feature_list)
     df = pd.DataFrame(dict_quantities)
+    if redshift_cut is not None:
+        df = df[df["redshift_true"] < redshift_cut]
     df.to_parquet(path_out)
