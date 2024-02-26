ebd1a30f6a3d7f0a744aacfc8b4b908a9196f525
corentinravoux <corentin.ravoux.research@gmail.com>
2024-02-26 11:04:40 +0100

    correct implementation of footprints

--- desidescsn/footprint.py
+++ desidescsn/footprint.py
@@ -1,33 +1,69 @@
+import fitsio
 import healpy as hp
 import numpy as np
+from astropy import units as u
+from astropy.coordinates import SkyCoord
+from astropy.table import Table
 from desimodel.footprint import is_point_in_desi
 
 
-def IndexToDeclRa(index, nside):
-    theta, phi = hp.pixelfunc.pix2ang(nside, index)
-    return np.degrees(np.pi * 2.0 - phi), -np.degrees(theta - np.pi / 2.0)
+def get_desi_mask(
+    desi_footprint_file,
+    ra,
+    dec,
+    program="BRIGHT",
+):
+    tiles = Table.read(desi_footprint_file)
+    selection = (tiles["IN_DESI"] == 1) & (tiles["PROGRAM"] == program)
+    tiles_selected = tiles[selection]
+    return is_point_in_desi(tiles_selected, ra, dec)
 
 
-def DeclRaToIndex(RA, DEC, nside):
-    return hp.pixelfunc.ang2pix(nside, np.radians(-DEC + 90.0), np.radians(360.0 - RA))
+def get_desi2_mask(
+    ra,
+    dec,
+    variant=0,
+):
+    mask = (ra < 50) & (-15 < dec) & (dec < 3)
+    mask |= (ra > -50 + 360) & (-15 < dec) & (dec < 3)
+    mask |= (ra > 130) & (ra < 220) & (-15 < dec) & (dec <= 0)
+    mask |= (ra > 120) & (ra < 250) & (0 <= dec) & (dec < 15)
+    return mask
 
 
-def plot_property(property_name, tiles_selected, npix, nside, radius):
-    property_map = np.full(npix, np.nan)
-    for i in range(len(tiles_selected)):
-        ind = DeclRaToIndex(tiles_selected["RA"][i], tiles_selected["DEC"][i], nside)
-        vec = hp.pix2vec(nside, ind)
-        ipix_disc = hp.query_disc(nside=nside, vec=vec, radius=np.radians(radius))
-        property_map[ipix_disc] = tiles_selected[property_name][i]
-
-    hp.mollview(
-        (property_map), hold=True, nest=False, notext=True, fig=1, title=property_name
+def get_crs_mask(crs_footprint_file, nside):
+    lsm_map = fitsio.FITS(crs_footprint_file)[1]["LSM"][:]
+    lsm_map_resolved = hp.pixelfunc.ud_grade(
+        lsm_map,
+        nside,
+        pess=False,
+        order_in="NEST",
+        order_out=None,
     )
-    hp.graticule()
+    mask = lsm_map_resolved != 0.0
+    return mask
 
 
-def get_desi_footprint(nside, desi_footprint_file):
+def get_4hs_mask(
+    ra,
+    dec,
+):
+    c = SkyCoord(ra=ra * u.degree, dec=dec * u.degree, frame="icrs").galactic
+    b = np.array([c[i].b.value for i in range(len(c))])
+
+    mask = dec < 0
+    mask &= np.abs(b) > 20.0
+
+    return mask
+
+
+def create_healpix_map(nside):
     npix = hp.nside2npix(nside)
-    index = np.arange(npix)
-    ra, dec = IndexToDeclRa(index, nside)
-    mask = is_point_in_desi(tiles_selected, ra, dec)
+    ra, dec = hp.pix2ang(
+        nside,
+        range(npix),
+        nest=True,
+        lonlat=True,
+    )
+    hpx_map = np.zeros(npix)
+    return ra, dec, hpx_map
